dd2a0fb611519c7a34fbcb5fb44568328b6db2c4
K.DH3 <30466064+331leo@users.noreply.github.com>
2021-01-26 21:43:49 +0900

    update: embed generator, member_remove

--- src/template/event_embed_gen.py
+++ src/template/event_embed_gen.py
@@ -5,9 +5,21 @@ import utils
 KST = datetime.timedelta(hours=9)
 
 def member_join(member, guild_config):
-    timestamp = datetime.datetime.utcnow()
-    embed = discord.Embed(title=f'{member} 님이 들어오셨어요!', description=f"{guild_config['member_join_description']}", color=utils.colormap['aqua'],timestamp=timestamp)
+    embed = discord.Embed(title=f"{member} 님이 들어오셨어요!",
+                          description=f"{guild_config['member_join_description'].format(guild_name=member.guild.name)}",
+                          color=utils.colormap["aqua"],
+                          timestamp=datetime.datetime.utcnow())
+    embed.set_thumbnail(url=f"{str(member.avatar_url)}")
+    embed.add_field(name="계정 생성일", value=f"{str(member.created_at + KST)[:-10]}", inline=True)
+    embed.set_footer(text=f"멤버로그 설정: {config.COMMAND_PREFIXS[0]}멤버로그")
+    return embed
+def member_remove(member, guild_config):
+    embed = discord.Embed(title=f"{member}님이 나가셨어요:cry:",
+                          description=f"{guild_config['member_remove_description'].format(guild_name=member.guild.name)}",
+                          color=utils.colormap["hotpink"],
+                          timestamp=datetime.datetime.utcnow())
     embed.set_thumbnail(url=f"{str(member.avatar_url)}")
     embed.add_field(name='계정 생성일', value=f'{str(member.created_at + KST)[:-10]}', inline=True)
+    embed.add_field(name='서버 참가일', value=f'{str(member.joined_at + KST)[:-10]}', inline=True)
     embed.set_footer(text=f"멤버로그 설정: {config.COMMAND_PREFIXS[0]}멤버로그")
     return embed
