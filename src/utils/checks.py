115c19e1eebbdf4101f0227ff12c8a2b275cd3d9
K.DH3 <30466064+331leo@users.noreply.github.com>
2021-01-26 21:53:53 +0900

    feat: permission control

---
+++ src/utils/checks.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import motor.motor_asyncio
+from . import exceptions
+class permissions:
+    def __init__(self, db: motor.motor_asyncio.AsyncIOMotorClient):
+        self.db = db
+    
+    async def registered(self, ctx):
+        db_user = await db.users.find_one({"discordId": ctx.author.id})
+        if db_user:
+            return True
+        raise exceptions.PermError.NotRegistered
+    
+    async def master(self, ctx):
+        db_user = await db.users.find_one({"discordId": ctx.author.id})
+        if not db_user:
+            raise exceptions.PermError.NotRegistered
+        if not db_user['flags']['master']:
+            raise exceptions.PermError.NotBotMaster
+
+    async def blacklist(self, ctx):
+        db_user = await db.users.find_one({"discordId": ctx.author.id})
+        if not db_user:
+            raise exceptions.PermError.NotRegistered
+        if db_user['flags']['ban']:
+            raise exceptions.PermError.BlacklistedUser
+    
--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -1,7 +1,19 @@
 import discord
 import asyncio
+from discord.ext import commands
 
 class DB_NO_GUILD_DOCUMENT(Exception):
     def __init__(self, bot, guild):
         bot.logger.exception(f"No such DB Document for: {guild.name}, {guild.id}")
         super().__init__(f"No DB Document for: {guild.name}, {guild.id}")
+
+class PermError:
+    class NotRegistered(commands.CheckFailure):
+        def __str__(self):
+            return "Not registered User"
+    class NotBotMaster(commands.CheckFailure):
+        def __str__(self):
+            return "Not Master User"
+    class BlacklistedUser(commands.CheckFailure):
+        def __str__(self):
+            return "BlackListed User"
