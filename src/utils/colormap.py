e33db43c2099a959759f5ff4e953721b782c2b8e
K.DH3 <331leo@outlook.com>
2021-01-27 14:14:47 +0900

    update: colormaping table

--- src/utils/colormap.py
+++ src/utils/colormap.py
@@ -1,5 +1,11 @@
 colormap = {
     "aqua": 0x00f1ff,
     "hotpink": 0xFF69B4,
-    "yellowgreen":0xADFF2F
+    "yellowgreen": 0xADFF2F,
+    "red": 0xff0000,
+    "purple": 0x800080,
+    "skyblue": 0xadd8e6,
+    "lightgreen": 0x90ee90,
+    "lightpink": 0xffc0cb,
+    "yellow": 0xfcf794
 }
